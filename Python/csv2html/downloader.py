1097164a4110f077ece650d97f12e64adbef3611
Fukusan <Fuku_georgius@fukuikousuke-no-MacBook-Air.local>
2019-12-17 06:41:00 +0900

    途中経過

--- Python/csv2html/downloader.py
+++ Python/csv2html/downloader.py
@@ -9,6 +9,8 @@ from io import IO
 from reader import Reader
 from table import Table
 
+import requests
+
 class Downloader(IO):
 	"""ダウンローダ：CSVファイル・画像ファイル・サムネイル画像ファイルをダウンロードする。"""
 
@@ -21,15 +23,17 @@ class Downloader(IO):
 
 	def download_csv(self):
 		"""情報を記したCSVファイルをダウンロードする。"""
-
+		a_file = requests.get(Attributes.csv_url(Attributes.class))
 		return
 
 	def download_images(self, image_filenames):
 		"""画像ファイル群または縮小画像ファイル群をダウンロードする。"""
-
+		an_image = requests.get(Attributes.base_url + 'image_filenames')
 		return
 
 	def perform(self):
 		"""すべて（情報を記したCSVファイル・画像ファイル群・縮小画像ファイル群）をダウンロードする。"""
+		self.download_csv()
+		self.download_images()
 
 		return
