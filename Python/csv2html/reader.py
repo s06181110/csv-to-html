017a22cfca19b3ab8a9ee616b24ee65e847e7af9
Fukui <fuku_georgius@icloud.com>
2019-12-17 16:14:30 +0900

    途中経過

--- Python/csv2html/reader.py
+++ Python/csv2html/reader.py
@@ -20,8 +20,8 @@ class Reader(IO):
 		"""ダウンロードしたCSVファイルを読み込む。""" 
 		first = True
 		filename = os.path.join(self.attributes().base_directory(), csv_url.split('/')[-1])
-		with codecs.open(filename, 'r', 'utf-8') as a_filename:
-			for a_line in filename:
+		with codecs.open(filename, 'r', 'utf-8') as a_file:
+			for a_line in a_file:
 				a_string = a_line.split()[0]
 				if first :
 					Table(attributes.names, a_line)
