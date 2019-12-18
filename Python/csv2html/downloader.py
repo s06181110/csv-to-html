f5c2f00bac04a425ba0c04e781584eef48f7d492
biva8710 <s06181110@gmail.com>
2019-12-18 13:17:19 +0900

    update downloader

--- Python/csv2html/downloader.py
+++ Python/csv2html/downloader.py
@@ -9,8 +9,6 @@ from io import IO
 from reader import Reader
 from table import Table
 
-import requests
-
 class Downloader(IO):
 	"""ダウンローダ：CSVファイル・画像ファイル・サムネイル画像ファイルをダウンロードする。"""
 
@@ -23,17 +21,32 @@ class Downloader(IO):
 
 	def download_csv(self):
 		"""情報を記したCSVファイルをダウンロードする。"""
-		a_file = requests.get(Attributes.csv_url(Attributes.class))
+		csv_url = self.attributes().csv_url()
+		print("From: " + csv_url)
+		filename = os.path.join(self.attributes().base_directory(), csv_url.split('/')[-1])
+		print("To: " + filename)
+		if os.path.exists(filename) and os.path.isfile(filename):
+			pass
+		else:
+			urllib.urlretrieve(csv_url, filename)
 		return
 
 	def download_images(self, image_filenames):
 		"""画像ファイル群または縮小画像ファイル群をダウンロードする。"""
-		an_image = requests.get(Attributes.base_url + 'image_filenames')
+		for image_file in image_filenames:
+			image_url = self.attributes().base_url() + image_file
+			print("From: " + image_url)
+			image_path = self.attributes().base_directory() + image_file
+			print("To: " + image_path)
+			urllib.urlretrieve(image_url,  image_path)
 		return
 
 	def perform(self):
 		"""すべて（情報を記したCSVファイル・画像ファイル群・縮小画像ファイル群）をダウンロードする。"""
 		self.download_csv()
-		self.download_images()
+		a_reader = Reader(self.table())
+		a_reader.perform()
+		self.download_images(self.table().image_filenames())
+		self.download_images(self.table().thumbnail_filenames())
 
 		return
